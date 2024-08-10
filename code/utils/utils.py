506845580a69b8a4144e89432022f9a38ddc7ccb
Chaitanya Gumudala <chaitanyagumudala961@gmail.com>
2024-08-10 12:05:01 +0200

    added documentations to the utils

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -6,8 +6,12 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
 
-
+# set_seed() function is used to set the seed for reproducibility
 def set_seed(seed):
+    """
+    Set the seed for reproducibility
+    input: seed value
+    """
     random.seed(seed)               
     np.random.seed(seed)            
     torch.manual_seed(seed)         
@@ -15,7 +19,13 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+# LoadDataset class is used to load the dataset for training
 class LoadDataset():
+    """
+    Load the dataset for training
+    input: dataset_path, batch_size
+    output: dataloader
+    """
     def __init__(self, dataset_path, batch_size):
         self.dataset_path = dataset_path
         self.batch_size = batch_size
@@ -30,7 +40,7 @@ class LoadDataset():
         except Exception as e:
             print(f"Error loading dataset: {e}")
 
-
+# plot_training_curve() function is used to plot the training curve
 def plot_training_curve(epochs, losses, augmentation_name, save_path):
     plt.figure(figsize=(10, 6))
     plt.plot(epochs, losses, label='Training Loss')
@@ -42,7 +52,13 @@ def plot_training_curve(epochs, losses, augmentation_name, save_path):
     plt.savefig(os.path.join(save_path, f'{augmentation_name}_training_curve.png'))
     plt.close()
 
+# RandomErasing class is used to randomly erase a rectangle from the image
 class RandomErasing:
+    """
+    Randomly erase a rectangle from the image
+    input: p(probability), value(value of the pixel)
+    output: img
+    """
     def __init__(self, p=0.5, value=0):
         self.p = p
         self.value = value
